bd9ed2a663737885e29b6f8b5e1510f364e06c0e
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2022-08-02 17:25:15 +0200

    upd only image ok !

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,13 +3,20 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { diskStorage } from 'multer';
 
 @Controller('api/users')
 export class UsersController {
@@ -18,6 +25,27 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+  @Post('image')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploadedFiles',
+        filename: (req, file, cb) => {
+          console.log(file);
+          const mimetype = file.mimetype.split('/')[1];
+          const fileName =
+            file.originalname + '-' + Date.now() + '.' + mimetype;
+          cb(null, fileName);
+        },
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
   }
 
   @Get()
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,21 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/user.schema';
+import { MulterModule } from '@nestjs/platform-express/multer';
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MulterModule.register({
+      dest: './uploadedFiles',
+      fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image')) {
+          cb(null, true);
+        } else {
+          cb(null, false);
+        }
+      },
+    }),
   ],
   controllers: [UsersController],
   providers: [UsersService],
